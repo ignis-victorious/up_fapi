8a66dbd212acacd3ca8d3161bfa8284a31b1184f
Emmanuele Levi <ignisvictorious@gmail.com>
2025-07-21 16:57:42 +0100

    Cannot find import

--- app/main.py
+++ app/main.py
@@ -1,156 +1,58 @@
 #  ________________________
 #  Import LIBRARIES
-from datetime import date
+from datetime import datetime
 
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI
 
 #  Import FILES
-from .models.models import Item, ModelName
+from .models.models import Order, OrderCreate
 
 #  ________________________
 
-
-# # Create an instance of the FastAPI class
-# app: FastAPI = FastAPI()
-
-# Create an instance with metadata
-app: FastAPI = FastAPI(
-    title="My First API",
-    description="A simple API built with FastAPI",
-    version="0.1.0",
-    docs_url="/documentation",  # Change the docs URL !!!
-    redoc_url="/redoc",
-)
+app: FastAPI = FastAPI()
 
 
-# Define a root endpoint.
-@app.get(path="/")
-def read_root() -> dict[str, str]:
-    return {"message": "Hello World"}
+@app.post(path="/orders/", response_model=Order)
+async def create_order(order: OrderCreate):
+    # In a real application, you would validate the user and address existence and save the order to a database
+    # For this example, we'll create a mock order
+    return Order(
+        id=1,
+        user_id=order.user_id,
+        shipping_address_id=order.shipping_address_id,
+        items=order.items,
+        notes=order.notes,
+        created_at=datetime.now(),
+    )
+    # total_amount = sum(item.unit_price * item.quantity for item in order.items)
+    # return Order(
+    #     id=1,
+    #     user_id=order.user_id,
+    #     shipping_address_id=order.shipping_address_id,
+    #     items=order.items,
+    #     notes=order.notes,
+    #     created_at=datetime.now(),
+    #     total_amount=total_amount
+    # )
 
 
-# # Define a path parameter endpoint.
-# @app.get(path="/items/{item_id}")
-# def read_item(item_id: int) -> dict[str, int]:
-#     return {"item_id": item_id}
+# @app.post(path="/orders/", response_model=Order)
+# async def create_order(order: OrderCreate) :
+#     # Convert OrderItems to OederIkenReeponse with calculated totals
+#     response_itens = 11
+#     for item in order.items:
+#     quantity item. quantity, unit price item.unit price,
+#     cotal-item-quantity = item.unit price
+#     # Calculate total amount
+#     total_amount = sum(item, total for item in response_items)
 
-
-# # Define a path + query parameter endpoint - Works with: http://127.0.0.1:8000/items/123?q=erre
-# @app.get(path="/items/{item_id}")
-# def read_item(item_id: int, q: str | None = None) -> dict[str, int | str | None]:
-#     return {"item_id": item_id, "q": q}
-
-
-# Define a POST endpoint with request body - Works with _ {"name": "Laptop", "price": 10, "is_offer": false}
-@app.post(path="/items/")
-def create_item(item: Item) -> Item:
-    return item
-
-
-# Works with: http://127.0.0.1:8000/items/123 (but not with 12345678)
-@app.get(path="/items/{item_id}")
-def read_item(
-    item_id: int = Path(default=..., title="The ID of the item", ge=0, le=1000),
-) -> dict[str, int]:  # With Path Parameter Validation
-    return {"item_id": item_id}
-
-
-# Multiple Values for the Same Parameter - Works with: http://127.0.0.1:8000/items/?q=erre&q=esse&q=emme
-@app.get(path="/items/")
-def read_items(q: list[str] = Query(default=None)) -> dict[str, list[str]]:
-    return {"q": q}
-
-
-# #  Boolean Type Conversion
-# @app.get(path="/items/")
-# def read_items(featured: bool = False) -> dict[str, str]:
-#     if featured:
-#         return {"featured": "Get only featured items"}
-#     return {"featured": "Get all items"}
-
-
-# #  Works with: http://127.0.0.1:8000/items/?q=aaa but not with ?q=aa or ?q=a1a or ...
-# @app.get(path="/items/")
-# def read_items(
-#     q: str | None = Query(
-#         default=None,  # Default value
-#         min_length=3,  # Minimum length
-#         max_length=50,  # Maximum length
-#         regex="^[a-z]+$",  # Regular expression pattern
-#         title="Query string",  # Title for documentationdescription="Query string for filtering items" # Description for does
-#     ),
-# ) -> dict[str, list[dict[str, str]]]:
-#     results: dict[str, list[dict[str, str]]] = {
-#         "items": [{"item_id": "Foo"}, {"item_id": "Bar"}]
-#     }
-#     if q:
-#         # results.update({"q": q})
-#         results["items"].append({"item_id": q})
-#     return results
-
-
-#  Works with: http://127.0.0.1:8000/items/?required_query=erre and http://127.0.0.1:8000/items/?required_query=erre&optional_query=esse
-# @app.get(path="/items/")
-# def read_items(
-#     required_query: str,  # Required parameter (no default value)
-#     optional_query: str | None = None,  # Optional parameter
-# ) -> dict[str, str]:
-#     results: dict[str, str] = {"required": required_query}
-#     if optional_query:
-#         results.update({"optional": optional_query})
-#     return results
-
-
-# #  Query Parameters - Worls with: http://127.0.0.1:8000/items/?skip=20&limit=50
-# @app.get(path="/items/")
-# def read_items(skip: int = 0, limit: int = 10) -> dict[str, int]:
-#     return {"skip": skip, "limit": limit}
-
-
-# # Works with: http://127.0.0.1:8000/items/12345678
-# @app.get(path="/items/{item_id}")
-# def read_item(item_id: int) -> dict[str, int]:  # Integer validation
-#     return {"item_id": item_id}
-
-
-# Works with: http://127.0.0.1:8000/users/user1
-@app.get(path="/users/{user_id}")
-def read_user(user_id: str) -> dict[str, str]:  # String (default)
-    return {"user_id": user_id}
-
-
-#  Works with: http://127.0.0.1:8000/files/{file_path:path)?file_path=folder%2Ffolder%2Ffile
-@app.get(path="/files/{file_path:path)")
-def read_file(file_path: str) -> dict[str, str]:  # Path parameter containing slashes
-    return {"file_path": file_path}
-
-
-#  Predefined Values with Enum
-@app.get(path="/models/{model_name}")
-def get_model(model_name: ModelName) -> dict[str, ModelName | str]:
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-
-# #  Works with: http://127.0.0.1:8000/models/497f6eca-6276-4993-bfeb-53cbbbba6f08
-# @app.get(path="/models/{model_uuid}")
-# def get_model(model_uuid: UUID) -> dict[str, UUID]:  # UUID validation
-#     print(f"model uiid: {model_uuid}")
-#     return {"model_uuid": model_uuid}
-
-
-#  This works: http://127.0.0.1:8000/events/2025-01-01
-@app.get(path="/events/{event_date}")
-def get_events(event_date: date) -> dict[str, date]:  # Date validation (YYYY-MM-DD)
-    return {"event_date": event_date}
-
-
-# def main():
-#     print("Hello from up-fapi!")
-
-
-# if __name__ == "__main__":
-#     main()
+# # Create and return the response object
+# return OrderResponse (
+#     id=1,
+#     user_id=order.user_id,
+#     shipping_address_id=order.shipping_address_id,
+#     items=response_items,
+#     notes=order.notes,
+#     created_at=datetime.now() ,
+#     total_amount=total_amount
+#     )
--- app/models/models.py
+++ app/models/models.py
@@ -1,21 +1,152 @@
 #  ________________________
 #  Import LIBRARIES
-from enum import Enum
+# from pydantic import BaseModel, Field, Emailstr, model_validator
+from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, model_validator
 
 #  Import FILES
 #  ________________________
 
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+# class Address(BaseModel):
+#     street: str
+#     city: str
+#     state: str
+#     zip_code: str
+#     country: str
 
 
-# Define a Pydantic model for request body validation
-class Item(BaseModel):
+# class User(BaseModel):
+#     id: int
+#     name: str
+#     email: str
+#     address: Address
+
+
+# # Example use
+# user: User = User(
+#     id=1,
+#     name="John Doe",
+#     email="john@example.com",
+#     address=Address(
+#         street="123 Main St",
+#         city="Anytown",
+#         state="CA",
+#         zip_code="12345",
+#         country="USA",
+#     ),
+# )
+
+# print("User: ")
+# print(user.model_dump_json())
+
+
+class Tag(BaseModel):
+    id: int
     name: str
-    price: float
-    is_offer: bool | None = None
+
+
+class Post(BaseModel):
+    id: int
+    title: str
+    content: str
+    tags: list[Tag] = []
+
+
+# Example
+post: Post = Post(
+    id=1,
+    title="Hello World",
+    content="This is my first post",
+    tags=[
+        Tag(id=1, name="python"),
+        Tag(id=2, name="pydantic"),
+        Tag(id=3, name="fastapi"),
+    ],
+)
+
+print("Post: ")
+print(post.model_dump_json())
+
+
+class Comment(BaseModel):
+    id: int
+    text: str
+    replies: list["Comment"] = []
+
+
+Comment.model_rebuild()  # Required in Pydantic V2 for recursive models
+
+
+# Example
+comment: Comment = Comment(
+    id=1,
+    text="Great article!",
+    replies=[
+        Comment(id=2, text="Thanks!"),
+        Comment(id=3, text="I agree!", replies=[Comment(id=4, text="Me too!")]),
+    ],
+)
+
+print("Comment: ")
+print(comment.model_dump_json())
+
+
+#  ORDER
+class AddressCreate(BaseModel):
+    street: str
+    city: str
+    state: str
+    zip_code: str
+    country: str
+
+
+class Address(AddressCreate):
+    id: int
+
+
+class OrderItem(BaseModel):
+    product_id: int
+    quantity: int = Field(gt=0)
+    unit_price: float = Field(gt=0)
+
+    @model_validator(mode="after")
+    def calculate_total(self) -> "OrderItem":
+        self.total: float = self.quantity * self.unit_price
+        return self
+
+
+class OrderCreate(BaseModel):
+    user_id: int
+    shipping_address_id: int
+    items: list[OrderItem]
+    notes: str | None = None
+
+
+class Order(OrderCreate):
+    id: int
+    created_at: datetime = datetime.now()
+    total_amount: float
+
+    @model_validator(mode="after")
+    def calculate_total_amount(self) -> "Order":
+        self.total_amount = sum(item.quantity * item.unit_price for item in self.items)
+        return self
+
+
+# {
+#     "user_id": 1,
+#     "shipping_address_id": 1,
+#     "items": [{"product_id": 1, "quantity": 10, "unit_price": 1.99}],
+#     "notes": "This is a note",
+# }
+
+# class OrdeResponse(BaseModel):
+#     id: int
+#     user_id: int
+#     shipping_address_id: int
+#     items:  list[OrdeRItemResponse]
+#     notes: str|None = None
+#     created_at: datetime
+#     total_amount: float
